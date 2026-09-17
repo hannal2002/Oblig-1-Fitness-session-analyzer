4596675d2fe1afa429839f424134a075997c08f9
Hanna Linnea Østern <haost7167@oslomet.no>
2026-09-17 09:22:22 +0200

    kobler participants til å ha referanse målinger

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 class Participant:
     def _init_(self, name): #En deltaker må ha et navn
         self.name = name
+        self.reference_measurements = reference_measurements #Composition: en participant har referansemålinger
 
 class ReferenceMeasurements:
     def _init_(self, heart_rate, temperature, activity_level): #Målingene inneholder puls, temp, aktivitetsnivå
@@ -8,8 +9,19 @@ class ReferenceMeasurements:
         self.temperature = temperature
         self.activity_level = activity_level
 
-class Observation:
-    pass
+class Observation: #Inneholder alle de forskjellige observasjonene
+    def _init_(self, timestamp, heart_rate, skin_response, temperature, acitvity_level, signal_quality):
+        self.timestamp = timestamp
+        self.heart_rate = heart_rate
+        self.skin_response = skin_response
+        self.temperature = temperature
+        self.activity_level = acitvity_level
+        self.signal_quality = signal_quality
 
-class Session: 
-    pass
+class Session: #Composition: session inneholder observation-objekter
+    def _init_(self, participant):
+        self.participant = participant
+        self.observations = [] #I hver sesjon er det observert forskjellig ting. Lagres her 
+    
+    def add_observation(self, observation):
+        self.observations.append(observation) #Legger observasjonene i lista
