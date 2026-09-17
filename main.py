d5278462febf552ed7bdd1a8c386d3bd2cb887f6
Hanna Linnea Østern <haost7167@oslomet.no>
2026-09-17 09:13:14 +0200

    Deltakernes målinger inneholder puls, temperatur og aktivitetsnivå

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 class Participant:
-    pass
+    def _init_(self, name): #En deltaker må ha et navn
+        self.name = name
 
 class ReferenceMeasurements:
-    pass
+    def _init_(self, heart_rate, temperature, activity_level): #Målingene inneholder puls, temp, aktivitetsnivå
+        self.heart_rate = heart_rate
+        self.temperature = temperature
+        self.activity_level = activity_level
 
 class Observation:
     pass
