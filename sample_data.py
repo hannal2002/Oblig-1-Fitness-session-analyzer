81c01666dada750b254bb69e986840e4e9eb673b
Hanna Linnea Østern <haost7167@oslomet.no>
2026-09-17 10:16:05 +0200

    Lagt til testdata for resting, moderate og high activity level

---
+++ sample_data.py
@@ -0,0 +1,58 @@
+#Tester med ulike scenario
+
+resting_data = [ #Også aantakelser
+    {
+        "timestamp": 1, 
+        "heart_rate": 70,
+        "skin_response": 1.2, 
+        "temperature": 32.5,
+        "activity_level": 0.1,
+        "signal_quality": 0.95
+    },
+    {
+        "timestamp": 2, 
+        "heart_rate": 72,
+        "skin_response": 1.3, 
+        "temperature": 32.6,
+        "activity_level": 0.15,
+        "signal_quality": 0.94
+    }
+]
+
+moderate_date = [
+    {
+        "timestamp": 1, 
+        "heart_rate": 105,
+        "skin_response": 2.0, 
+        "temperature": 33.0,
+        "activity_level": 0.4,
+        "signal_quality": 0.95
+    },
+    {
+        "timestamp": 2, 
+        "heart_rate": 115,
+        "skin_response": 2.3, 
+        "temperature": 33.2,
+        "activity_level": 0.5,
+        "signal_quality": 0.96
+    }
+]
+
+high_data = [
+    {
+        "timestamp": 1, 
+        "heart_rate": 155,
+        "skin_response": 3.1, 
+        "temperature": 33.8,
+        "activity_level": 0.8,
+        "signal_quality": 0.94
+    },
+    {
+        "timestamp": 2, 
+        "heart_rate": 165,
+        "skin_response": 3.4, 
+        "temperature": 34.0,
+        "activity_level": 0.9,
+        "signal_quality": 0.95
+    }
+]
