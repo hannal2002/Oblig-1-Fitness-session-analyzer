cc37d5c96b3b2ecf00b8415ce0d352d337c2a8ac
Hanna Linnea Østern <haost7167@oslomet.no>
2026-09-17 10:21:50 +0200

    lagt til testdata for recovery også med synkende verdier for puls og aktivitetsnivå

--- sample_data.py
+++ sample_data.py
@@ -56,3 +56,30 @@ high_data = [
         "signal_quality": 0.95
     }
 ]
+
+recovery_data = [
+    {
+        "timestamp": 1, 
+        "heart_rate": 160,
+        "skin_response": 3.2, 
+        "temperature": 34.0,
+        "activity_level": 0.85,
+        "signal_quality": 0.95
+    },
+    {
+        "timestamp": 2, 
+        "heart_rate": 150,
+        "skin_response": 3., 
+        "temperature": 33.8,
+        "activity_level": 0.7,
+        "signal_quality": 0.94
+    },
+    {
+        "timestamp": 3, 
+        "heart_rate": 125, #Synkende
+        "skin_response": 2.5, 
+        "temperature": 33.4,
+        "activity_level": 0.4, #Synkende
+        "signal_quality": 0.96
+    }
+]
